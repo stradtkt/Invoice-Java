5735c66553da73fe96d85cd29a6324952775f16d
Kevin Stradtman <stradtkt@gmail.com>
2018-11-04 16:04:01 -0500

    Added the Invoice file

--- src/invoice/Invoice.java
+++ src/invoice/Invoice.java
@@ -1,5 +1,34 @@
 package invoice;
 
-public class Invoice {
+import java.util.ArrayList;
 
+public class Invoice {
+	private Address address;
+	private ArrayList<LineItem> items;
+	
+	public Invoice(Address address) {
+		items = new ArrayList<LineItem>();
+		this.address = address;
+	}
+	public void add(Product product, int quantity) {
+		LineItem anItem = new LineItem(product, quantity);
+		items.add(anItem);
+	}
+	private String format() {
+		String in = "                                    I N V O I C E \n\n"
+				+ address.format()
+				+ String.format("\n\n%-30s%8s%Ss%8s\n", "Name", "Price", "Qty", "Total");
+		for(LineItem item : items) {
+			in = in + item.format() + "\n";
+		}
+		in = in + String.format("\nAMOUNT DUE: $%8.2f", getAmountDue());
+		return in;
+	}
+	private double getAmountDue() {
+		double amountDue = 0;
+		for(LineItem item: items) {
+			amountDue = amountDue + item.getTotalPrice();
+		}
+		return amountDue;
+	}
 }
