601ca4b99a0a6377d3d77d0740f76c368610ae3b
Kevin Stradtman <stradtkt@gmail.com>
2018-11-04 15:02:54 -0500

    Added the address file

--- src/invoice/Address.java
+++ src/invoice/Address.java
@@ -1,5 +1,21 @@
 package invoice;
 
 public class Address {
-
+	private String name;
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	
+	public Address(String name, String street, String city, String state, String zip) {
+		this.name = name;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	
+	public String format() {
+		return name + "\n" + street + "\n" + city + ", " + state + " " + zip;
+	}
 }
