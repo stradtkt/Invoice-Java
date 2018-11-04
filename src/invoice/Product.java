6f840ada23410bfa71677bfb8a6cfd52178495fd
Kevin Stradtman <stradtkt@gmail.com>
2018-11-04 15:41:57 -0500

    Added the product file

--- src/invoice/Product.java
+++ src/invoice/Product.java
@@ -1,5 +1,18 @@
 package invoice;
 
 public class Product {
-
+	private String name;
+	private double price;
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public double getPrice() {
+		return price;
+	}
 }
